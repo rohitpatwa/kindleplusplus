96f40c4cff52f1f2b94e052482f313790a4b7073
Ativeer <55151895+Ativeer@users.noreply.github.com>
2020-01-23 02:46:37 -0500

    BCC and HTML Added
    Complete BCC and html changes

--- sendMail.py
+++ sendMail.py
@@ -1,12 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 21 22:59:50 2020
 
-# Python 2.x
-
-# import smtplib
-# from email.MIMEMultipart import MIMEMultipart
-# from email.MIMEText import MIMEText
-
-
-# Python 3.x
+@author: 2ativ
+"""
 
 import smtplib
 from email.mime.multipart import MIMEMultipart
@@ -14,22 +11,27 @@ from email.mime.text import MIMEText
 from config import mailcfg 
 
  
-def sendMail(book_name, authors, note):
-	fromaddr = f'{mailcfg["fromname"]}<{mailcfg["fromaddr"]}>'
-						 
-	toaddr = mailcfg['toaddr']
-	msg = MIMEMultipart()
-	msg['From'] = fromaddr
-	msg['To'] = toaddr
+def sendmail(book_name, authors,note):
+    fromaddr = f'{mailcfg["fromname"]}<{mailcfg["fromaddr"]}>'		 
+    toaddr = mailcfg['toaddr']
+    msg = MIMEMultipart()
+    msg['From'] = fromaddr
+    sb = book_name.split()[0] if len(book_name.split()[0]) > 4 else book_name[:15] + '...'
+    msg['Subject'] = f"Trying with HTML | {sb}"
+    new_body=""
+    with open('/trial.txt',"r") as f:
+        data=f.readlines()
+    data[35]=sb
+    data[38]=authors
+    data[43]=note
+    new_body=""
+    for i in data:
+        new_body=new_body+i
+    msg.attach(MIMEText(new_body,'html'))
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(mailcfg['fromaddr'], mailcfg['psswd'])
+    text = msg.as_string()
+    server.sendmail(fromaddr, toaddr.split(','), text)
+    server.quit()
 
-	book_name = book_name.split()[0] if len(book_name.split()[0]) > 4 else book_name[:15] + '...'
-	msg['Subject'] = f"Highlight of the day! | {book_name}"
-	 
-	body = '<font face="Courier New, Courier, monospace">' + note + '</font>'
-	msg.attach(MIMEText(body,'html'))
-	server = smtplib.SMTP('smtp.gmail.com', 587)
-	server.starttls()
-	server.login(mailcfg['fromaddr'], mailcfg['psswd'])
-	text = msg.as_string()
-	server.sendmail(fromaddr, toaddr.split(','), text)
-	server.quit()
