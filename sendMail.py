250e114cd1731952d717696a62fee6eec2437aaf
rohitpatwa <rohitpatwa@gmail.com>
2020-01-23 02:57:16 -0500

    sendmail -> sendMail sb -> book_name Fix the subject of the mail

--- sendMail.py
+++ sendMail.py
@@ -11,17 +11,17 @@ from email.mime.text import MIMEText
 from config import mailcfg 
 
  
-def sendmail(book_name, authors,note):
+def sendMail(book_name, authors,note):
     fromaddr = f'{mailcfg["fromname"]}<{mailcfg["fromaddr"]}>'		 
     toaddr = mailcfg['toaddr']
     msg = MIMEMultipart()
     msg['From'] = fromaddr
     sb = book_name.split()[0] if len(book_name.split()[0]) > 4 else book_name[:15] + '...'
-    msg['Subject'] = f"Trying with HTML | {sb}"
+    msg['Subject'] = f"Highlight of the day!| {sb}"
     new_body=""
-    with open('/trial.txt',"r") as f:
+    with open('trial.txt',"r") as f:
         data=f.readlines()
-    data[35]=sb
+    data[35]=book_name
     data[38]=authors
     data[43]=note
     new_body=""
