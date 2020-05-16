f1decfb16c70d156eba0af67a6c72d79e475c7b6
rohitpatwa <rohitpatwa@gmail.com>
2020-05-16 19:54:19 -0400

    mechanish enabled to fire mail after 24 hours whenever the laptop is started

--- code/emailer.py
+++ code/emailer.py
@@ -3,7 +3,6 @@ get a random note and send an email through SendMail utility.
 Update the last send info too.
 """
 
-
 from sendMail import sendMail as mailer
 from mongoOps import MongoObject
 from random import randint
@@ -21,7 +20,7 @@ args = parser.parse_args()
 
 
 
-def get_random_note(test):
+def get_random_note(test, mongo):
 	query = {'last_sent':{'$lte': int(time()) - 1296000}}
 	count = mongo.db['notes'].count_documents(query)
 	rand = randint(0,count-1)
@@ -31,9 +30,28 @@ def get_random_note(test):
 			mongo.update_last_sent(d['_id'])
 		return d
 
-mongo = MongoObject()
-random_note = get_random_note(args.test)
-book_doc = mongo.db['books'].find_one({'_id':random_note['book_id']})
-book_name, authors = book_doc['book_name'], ','.join(book_doc['authors'])
-mailer(book_name, authors, random_note['note'], args.test)
-print(f'Email sent at {str(datetime.now()).split(".")[0]}')
+
+def fire():
+	mongo = MongoObject()
+	random_note = get_random_note(args.test, mongo)
+	book_doc = mongo.db['books'].find_one({'_id':random_note['book_id']})
+	book_name, authors = book_doc['book_name'], ','.join(book_doc['authors'])
+	mailer(book_name, authors, random_note['note'], args.test)
+	print(f'Email sent at {str(datetime.now()).split(".")[0]}')
+
+
+if __name__=="__main__":
+	files = os.listdir()
+	if 'emailer.py' in files:
+		f = open('mailer_logs.txt', 'r')
+		# x = f.readlines()[-1][14:-1]
+		# print(x)
+		last_sent = datetime.strptime(f.readlines()[-1][14:-1], '%Y-%m-%d %H:%M:%S')
+		now = datetime.now()
+		if (now-last_sent).total_seconds()/3600 > 24:
+			fire()
+		else:
+			pass
+
+
+	
