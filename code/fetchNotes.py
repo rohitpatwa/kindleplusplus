c197c70b23ae9f829b8e6cad98b2036f71e9b557
rohitpatwa <rohitpatwa@gmail.com>
2020-05-16 20:54:12 -0400

    Mechanism implemented to download and parse notes every 3 days!

--- code/fetchNotes.py
+++ code/fetchNotes.py
@@ -1,8 +1,12 @@
 import imaplib
 import email
 import os
+import sys
 from config import mailcfg
 import re
+from run import process 
+from datetime import datetime
+os.chdir(sys.path[0])
   
 mail= imaplib.IMAP4_SSL('imap.gmail.com')
 
@@ -11,6 +15,22 @@ mail.select("Inbox")
 typ, msgs = mail.search(None, 'Subject', '"{}"'.format('Your Kindle Notes From'))
 msgs = msgs[0].split()
 
+# iterate through emails from latest to oldest
+msgs = msgs[-5:][::-1]
+
+
+def clean_file_name(fname):
+	allowed_chars = list(range(48, 58)) + list(range(65, 91)) + list(range(97, 123))
+	allowed_special_chars = [ord(x) for x in '`~!@#$%&*()-_=+[{]}|;:?.>,< ']
+	allowed_chars += allowed_special_chars
+	return ''.join(list(filter(lambda x:ord(x) in allowed_chars, fname)))
+
+
+
+
+print(f'Downloading and parsing started at : {str(datetime.now()).split(".")[0]}\n')
+
+unique_books = set()
 for num in msgs:
 	raw_email = mail.fetch(num,'(RFC822)')
 	raw_email_str = raw_email[1][0][1].decode('UTF-8')
@@ -25,7 +45,14 @@ for num in msgs:
 			continue
 		if part.get('Content-Disposition') is None:
 			continue
-		fileName = part.get_filename()
+		fileName = clean_file_name(part.get_filename())
+		
+		# Do not download and parse same book multiple times
+		if fileName in unique_books:
+			continue
+		else:
+			unique_books.add(fileName)
+
 		if bool(fileName) and 'csv' in fileName:
 
 			fileName = fileName.replace('.csv', '') + f'___{sender_info}' + '.csv'
@@ -37,3 +64,7 @@ for num in msgs:
 			subject = str(email_message).split("Subject: ", 1)[1].split("\nTo:", 1)[0]
 			print('Downloaded : "{file}"'.format(file=fileName))
 
+
+			print('Parsing book...')
+			process(filePath)
+print('\n'*3)
--- code/run.py
+++ code/run.py
@@ -3,16 +3,20 @@ from mongoOps import MongoObject
 import sys
 
 
-path = sys.argv[1]
+def process(path):
+	parser = Parser(path)
+	parser.parse()
 
-parser = Parser(path)
-parser.parse()
+	mongo = MongoObject()
 
-mongo = MongoObject()
+	mongo.insert_document(parser.book_info, 'books')
 
-mongo.insert_document(parser.book_info, 'books')
+	mongo.insert_document(parser.user, 'users')
 
-mongo.insert_document(parser.user, 'users')
+	for h in parser.highlights:
+	    mongo.insert_document(h, 'notes')
 
-for h in parser.highlights:
-    mongo.insert_document(h, 'notes')
+
+if __name__=='__main__':
+	path = sys.argv[1]
+	process(path)
