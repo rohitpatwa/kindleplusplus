29ef707152e9fd8b0d2a7b581eb7f27427a021bd
rohitpatwa <rohitpatwa@gmail.com>
2021-03-16 09:39:01 -0400

    Print exception in logs in emailer

--- code/emailer.py
+++ code/emailer.py
@@ -41,15 +41,18 @@ def fire():
 
 
 if __name__=="__main__":
-	files = os.listdir()
-	if 'mailer_logs.txt' in files:
-		f = open('mailer_logs.txt', 'r')
-		last_sent = datetime.strptime(f.readlines()[-1][14:-1], '%Y-%m-%d %H:%M:%S')
-		now = datetime.now()
-		if (now-last_sent).total_seconds()/3600 > 23:
-			fire()
-		else:
-			pass
+	try:
+		files = os.listdir()
+		if 'mailer_logs.txt' in files:
+			f = open('mailer_logs.txt', 'r')
+			last_sent = datetime.strptime(f.readlines()[-1][14:-1], '%Y-%m-%d %H:%M:%S')
+			now = datetime.now()
+			if (now-last_sent).total_seconds()/3600 > 23:
+				fire()
+			else:
+				pass
 
-	else:
-		fire()
+		else:
+			fire()
+	except Exception as e:
+		print(e)
